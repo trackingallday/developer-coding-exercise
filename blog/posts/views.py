dab6ba69356a90906ec4f78e93da44ba3d0e8889
trackingallday <rimu.boddy@sitem8.com>
2023-05-23 17:02:38 +0800

    add views - move stopwords to the model

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,29 +1,40 @@
-# Feel free to move this to a new file if you are carrying out the 'tags' calculation there
-stopWords = [
-    "#", "##", "a", "about", "above", "after", "again", "against", "all", "am",
-    "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
-    "before", "being", "below", "between", "both", "but", "by", "can't", "cannot",
-    "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
-    "down", "during", "each", "few", "for", "from", "further", "had", "hadn't",
-    "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
-    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how",
-    "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't",
-    "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my",
-    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other",
-    "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she",
-    "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that",
-    "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
-    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
-    "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
-    "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
-    "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't",
-    "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours",
-    "yourself", "yourselves"
-]
+import os
+from django.conf import settings
+from collections import Counter
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseServerError
+from .models import Post
+
 
 def post(request, slug):
-    pass
-
+    # the directory should really be an environment variable
+    filepath = os.path.join(settings.BASE_DIR, 'posts/posts', slug + '.md')
+    #try to find a matching post we can't find one, return a 404
+    try:
+        with open(filepath) as f:
+            post = Post(content=f.read())
+            return JsonResponse({
+                'content': post.only_content(),
+                'tags': post.tags(),
+                'titel': post.title(),
+            })
+    except FileNotFoundError:
+        return HttpResponseNotFound()
+    except Exception as e:
+        #probably a permissions error
+        return HttpResponseServerError()
+        
 
 def posts(request):
-    pass
+    posts = []
+    # the directory should really be an environment variable
+    # not checking for permissions errors or any of the other kinds of errors
+    for filename in os.listdir(os.path.join(settings.BASE_DIR, 'posts/posts')):
+        if filename.endswith('.md'):
+            filepath = os.path.join(settings.BASE_DIR, 'posts/posts', filename)
+            with open(filepath) as f:
+                post = Post(content=f.read(), slug=filename[:-3])
+                posts.append({
+                    'slug': post.slug,
+                    'title': post.title(),
+                })
+    return JsonResponse({'posts': posts})
