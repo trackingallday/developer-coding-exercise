fec042883855298ee4bf62c09dc42eb3eeeb20c6
trackingallday <rimu.boddy@sitem8.com>
2023-05-24 12:12:34 +0800

    make the views use the post models rather than textfiles

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,40 +1,14 @@
-import os
 from django.conf import settings
-from collections import Counter
-from django.http import JsonResponse, HttpResponseNotFound, HttpResponseServerError
+from django.http import JsonResponse, HttpResponseNotFound
 from .models import Post
 
 
 def post(request, slug):
-    # the directory should really be an environment variable
-    filepath = os.path.join(settings.BASE_DIR, 'posts/posts', slug + '.md')
-    #try to find a matching post we can't find one, return a 404
-    try:
-        with open(filepath) as f:
-            post = Post(content=f.read())
-            return JsonResponse({
-                'content': post.only_content(),
-                'tags': post.tags(),
-                'titel': post.title(),
-            })
-    except FileNotFoundError:
+    post = Post.objects.filter(slug=slug).first()
+    if not post:
         return HttpResponseNotFound()
-    except Exception as e:
-        #probably a permissions error
-        return HttpResponseServerError()
-        
+    return JsonResponse(post.toJSON(), safe=False)
 
 def posts(request):
-    posts = []
-    # the directory should really be an environment variable
-    # not checking for permissions errors or any of the other kinds of errors
-    for filename in os.listdir(os.path.join(settings.BASE_DIR, 'posts/posts')):
-        if filename.endswith('.md'):
-            filepath = os.path.join(settings.BASE_DIR, 'posts/posts', filename)
-            with open(filepath) as f:
-                post = Post(content=f.read(), slug=filename[:-3])
-                posts.append({
-                    'slug': post.slug,
-                    'title': post.title(),
-                })
-    return JsonResponse({'posts': posts})
+    posts_as_dicts = [p.toJSON() for p in Post.objects.all()]
+    return JsonResponse(posts_as_dicts, safe=False)
